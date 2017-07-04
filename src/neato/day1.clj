4a9b9efc1eb28ed9f5fa793b838703b8218d8cab
Amir Barylko <amir@barylko.com>
2017-07-03 20:56:53 -0700

    Added code reviews for day 1 and 4

--- src/neato/day1.clj
+++ src/neato/day1.clj
@@ -9,6 +9,11 @@
 (defn- gen-visited
   [from to]
   (let [distance (m/abs (- from to))]
+    ;; Instead of repeating `(take distance (iterate ...)` you could just
+    ;; decide which function to use `dec` or `inc` and then use it with
+    ;; `(take distance (iterate f (f from)))`
+    ;; Also this looks very much like a range
+    ;; maybe like `(range from to)`
     (if (< to from)
       (take distance (iterate dec (dec from)))
       (take distance (iterate inc (inc from))))))
@@ -28,6 +33,9 @@
   (+ (m/abs (- l1 l2))
      (m/abs (- r1 r2))))
 
+;; This implementation is Very hard to read
+;; Following the name it should add x1 and y1 to the coordinates but it is updating
+;; `:visited` as well ....
 (defn- add-to-coordinates
   [x1 y1 {x2 :x y2 :y total-visited :visited :as coord}]
   (let [new-x (+ x1 x2)
@@ -41,18 +49,26 @@
         coord)
       coord)))
 
+;; The name `get-direction` would suggest is some kind of query but is modifiying `coord` and adding more points
 (defn- get-direction [{:keys [towards value]} coord]
+  ;; `Using `case` you would have to repeat `(= towards)` each time
+  ;; Also maybe try to find a way to return the point and then
+  ;; apply the addition after
   (cond
     (= towards "N") (add-to-coordinates 0 (+ value) coord)
     (= towards "E") (add-to-coordinates (+ value) 0 coord)
     (= towards "S") (add-to-coordinates 0 (- value) coord)
     (= towards "W") (add-to-coordinates (- value) 0 coord)
+    ;; If you believe you need an `:else` then either provide
+    ;; a value that makes sense or throw an exception
     :else
     "get-direction issue"))
 
+;; This also looks like a `reduce` function
 (defn calc-coordinates [data]
-  (loop [old data
-         coordinates {:x 0 :y 0 :visited #{}}]
+  ;; You could use pattern matching here for old
+  ;; to get [x & xs]
+  (loop [old data coordinates {:x 0 :y 0 :visited #{}}]
     (if (empty? old)
       coordinates
       (recur
@@ -60,6 +76,11 @@
        (get-direction (first old) coordinates)))))
 
 (defn- assign-cardinal-direction [{:keys [direction value] :as data} facing]
+  ;; instead of `cond` you could use `case` and do something like
+  ;; `(case [direction value]
+  ;;     [\R \N] \E
+  ;;     [\L \N] \W
+  ;;     ....)`
   (cond
     (and (= direction "R")(= facing "N")) (assoc data :towards "E")
     (and (= direction "L")(= facing "N")) (assoc data :towards "W")
@@ -69,14 +90,22 @@
     (and (= direction "L")(= facing "S")) (assoc data :towards "E")
     (and (= direction "R")(= facing "W")) (assoc data :towards "N")
     (and (= direction "L")(= facing "W")) (assoc data :towards "S")
+    ;; If you believe you need an `:else` then either provide
+    ;; a value that makes sense or throw an exception
     :else
     "assign-cardinal-direction issue"))
 
+;; Using custom recursion (i.e. `loop`) is a fantastic tool but first we should make sure there's not already a well known function that does the same thing.
+;; Having a function that everybody knows how it works provides lots of benefits
+;; In this case it looks like a `reduce` function where you call `assign-cardinal-direction` in each step and collect the results in a list
 (defn- populate-cardinal-direction [data]
-  (loop [old data
-         new []
-         compass "N"]
+  ;; To use the pattern (first : rest) in `Clojure`
+  ;; you can use [x & xs]
+  ;; so you could do
+  ;; `(loop [[x & xs] data ....)`
+  (loop [old data new [] compass "N"]
     (let [point (-> old first (assign-cardinal-direction compass))]
+      ;; `old` can not be empty because `first` will return `nil`
       (if (empty? old)
         new
         (recur
@@ -86,12 +115,29 @@
 
 (defn- populate-map
   [x]
+  ;; why not just use `head` and then use characters \L n \R
   (let [d (-> x first str)
+        ;; for the value you can also use (-> x (.substring 1) read-string)
         v (->> x (re-find  #"\d+") Integer.)
         point {}]
+      ;; both `if` branches do the same thing
+      ;; there's no need to create an empty `{}` you can just return `{:direction d :value v}`
     (if (= "L" d)
       (assoc point :direction d :value v)
       (assoc point :direction d :value v))))
+
+;; instead of passing a boolean flag, we could just pass a function that is called at the end, and not _if_ is necessary
+
+;; The fn name _calculate_ is a bit ambiguos ... perhaps a name like "find-distance-to-hq" or "find-first-point-visited-twice" could help to understand better the intent
+
+;; Is a bit hard to tell what the function is doing... _populate-map_ could be called _parse-instructions_ (or something like that)
+
+;; instead of using `as->` you could do
+;; (->> (split x ...)
+;;      parse-instructions
+;;      populate-cardinal-direction
+;;      calc-coordinates
+;;      fn-that-goes-at-the-end)
 
 (defn calculate
   [data hq1?]
@@ -104,6 +150,7 @@
       (manhattan-distance {:x 0 :y 0} x)
       (manhattan-distance {:x 0 :y 0} (:hq2 x)))))
 
+;; This could be in the test. Is not used int the code
 (defn headquarters-two
   []
   (let [file "day1data.txt"]
@@ -112,6 +159,7 @@
         first
         (calculate false))))
 
+;; This could be in the test. Is not used int the code
 (defn headquarters-one
   []
   (let [file "day1data.txt"]
--- src/neato/day4.clj
+++ src/neato/day4.clj
@@ -17,17 +17,34 @@
 
 (defn keywordize-map [coll]
   (w/keywordize-keys
-   (into {} (for [[k v] coll]
-              [(str k) v]))))
+   (into {} (for [[k v] coll] [(str k) v]))))
 
+;; No need to add parethesis when is only a function without args
+;; (->> encrypted-name
+;;      frequencies
+;;      keywordize-map
+;;      .... )
+;;
+;; Sometimes is a bit more clear when you combine small functions very well known:
+;; (->> room-name
+;;      frequencies
+;;      (map (comp vec reverse))
+;;      (sort-by identity descendant-order)
+;;      (take 5)
+;;      (map last)
+;;      (apply str)
+;;      (= checksum)))
+;;
 (defn parse-checksum
   [encrypted-name checksum]
   (->> encrypted-name
-    (frequencies)
-    (keywordize-map)
-    (sort-map)
-    (take (count checksum))))
+       (frequencies)
+       (keywordize-map)
+       (sort-map)
+       (take (count checksum))))
 
+;; Usually functions that end in `?` mean they are predicates (functions that return a Boolean value)
+;; In this case, to return a boolean you don't need to add an `if`
 (defn real?
   [{e :encrypted-name c :checksum id :sector-id}]
   (if (->> (parse-checksum e c)
@@ -38,6 +55,12 @@
     (Integer. id)
     0))
 
+;; You don't need to use `assoc` on just one element, you can do it with multiple
+;; `(assoc k1 e1 k2 e2 ... kn en)`
+;; also you can just use the hashmap constructor
+;; `{:sector-id (re-find ...) :checksum (last ...) :encrypted-name (replace ..)}`
+;; Good job using `def` for the regex. I would perhaps keeps the regex close the functiona that uses them instead of at the top of the file.
+;; Even maybe put them inside of the function as `let`
 (defn parse-string
   [x]
   (-> {}
@@ -45,6 +68,8 @@
       (assoc :checksum (last (re-find checksum-regex x)))
       (assoc :encrypted-name (st/replace (re-find name-regex x) #"-" ""))))
 
+;; Why using `mapv` over `map` with just one collection?
+;; It looks a bit weird that you are doing `map real?` (expecting to map to boolean) and after you add the values ....
 (defn sum-of-IDs
   [coll]
   (->> coll
@@ -71,6 +96,12 @@
                 :sector-id id))
     nil))
 
+;; again `mapv` instead of `map` ... unless you really need a vector....
+;; Instead of `parse-dataset` you could use `clojure.string/split-lines`
+;; Is ok to do two mapping operations, but if you want composition you could write
+;; `(map (comp decrypt-room parse-string))`
+;; Avoid using generic names like `parse-string`. In this case is not a big problem because is a private function, but is better if the name of the function give a clear idea of what it does like `parse-string-into-room` or `string->room`
+
 (defn decoy-two
   []
   (->> (parse-dataset "day4data.txt")
@@ -78,6 +109,8 @@
        (mapv decrypt-room)
        (remove nil?)))
 
+;; again `mapv` instead of `map`
+;; `sum-of-ids` could be `(filter is-real?) (map :sector-id) (reduce +)` to describe better your intention and what's going on....
 (defn decoy-one
   []
   (->> (parse-dataset "day4data.txt")
